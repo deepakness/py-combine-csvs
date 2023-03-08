d0829c7e1521939696c50edfd7bf7f97b9b17141
deepakness <deepakkr342@gmail.com>
2023-03-08 09:02:33 +0530

    added sample files

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ import glob
 import pandas as pd
 
 # Set working directory to folder containing CSV files
-os.chdir("CSVs to Combine")
+os.chdir("CSVs")
 
 # Get list of all CSV files in folder
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
 # Combine all CSV files into one DataFrame
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
 
-# Write combined DataFrame to CSV file
-combined_csv.to_csv("Combined CSV/combined_csv.csv", index=False, encoding='utf-8-sig')
+# Create directory 'Combined CSV' if it doesn't exist
+if not os.path.exists('Combined'):
+    os.makedirs('Combined')
+
+# Write combined DataFrame to CSV file in directory 'Combined CSV'
+combined_csv.to_csv("Combined/combined_csv.csv", index=False, encoding='utf-8-sig')
